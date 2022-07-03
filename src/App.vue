313010263008599c62d2c37f6dc0b1723ff83ffa
Yi3512 <2399283079@qq.com>
2022-07-03 20:01:52 +0800

    Todo案例

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <todo-header @add="addFn" @setchecked="setcheckedFn"> </todo-header>
+    <todo-main :list="showlist" @del="delFn"></todo-main>
+    <todo-footer
+      :count="count"
+      @dele="dele"
+      @fliterdata="fliterdataFn"
+    ></todo-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoMain from "./components/TodoMain.vue";
+import TodoFooter from "./components/TodoFooter.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { TodoHeader, TodoMain, TodoFooter },
+  data() {
+    /*  第一步,铺设数据
+    1.通过父传子的方式 把数据传过去
+    2.子组件props 接受数据
+    2.使用v-for 来渲染数据
+    3.父组件传数据
+   */
+    return {
+      list: JSON.parse(localStorage.getItem("list")) || [],
+      getSel: "all",
+    };
+  },
+  methods: {
+    //添加
+    addFn(val) {
+      const id = this.list[this.list.length - 1]
+        ? this.list[this.list.length - 1].id + 1
+        : 100;
+      this.list.push({
+        name: val,
+        isDone: false,
+        id,
+      });
+    },
+    //删除
+    delFn(id) {
+      const index = this.list.findIndex((ele) => ele.id == id);
+      this.list.splice(index, 1);
+    },
+    //切换数据
+    fliterdataFn(val) {
+      console.log(val);
+      this.getSel = val;
+    },
+    //清空以=已完成
+    dele() {
+      this.list = this.list.filter((ele) => ele.isDone == false);
+    },
+    setcheckedFn(val) {
+      this.list.forEach((ele) => (ele.isDone = val));
+    },
+  },
+  computed: {
+    //剩余数
+    count() {
+      return this.list.filter((ele) => !ele.isDone).length;
+    },
+    //显 示
+    showlist() {
+      if (this.getSel == "no") {
+        return this.list.filter((ele) => !ele.isDone);
+      } else if (this.getSel == "yes") {
+        return this.list.filter((ele) => ele.isDone);
+      } else {
+        return this.list;
+      }
+    },
+  },
+  watch: {
+    list: {
+      deep: true,
+      handler(val) {
+        localStorage.setItem("list", JSON.stringify(val || []));
+      },
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less" scoped>
 </style>
---
+++ src/components/TodoMain.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="todo-list">
+    <!-- completed: 完成的类名 -->
+    <li :class="{ completed: item.isDone }" v-for="item in list" :key="item.id">
+      <div class="view">
+        <input class="toggle" type="checkbox" v-model="item.isDone" />
+        <label>{{ item.name }}</label>
+        <button class="destroy" @click="del(item.id)"></button>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    list: {
+      type: Array,
+      default: () => [], //默认值为数组
+    },
+  },
+  methods: {
+    del(id) {
+      this.$emit("del", id);
+    },
+  },
+};
+</script>
