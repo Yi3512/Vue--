94df6ee0191f7ea92dd6ec418296bb27e9692643
Yi3512 <2399283079@qq.com>
2022-07-04 21:17:36 +0800

    图书管理

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,30 @@
 <template>
   <div>
-    <todo-header @add="addFn" @setchecked="setcheckedFn"> </todo-header>
-    <todo-main :list="showlist" @del="delFn"></todo-main>
-    <todo-footer
-      :count="count"
-      @dele="dele"
-      @fliterdata="fliterdataFn"
-    ></todo-footer>
+    <head-fn></head-fn>
+    <form-fn :list='list'></form-fn>
+    <bot-tom></bot-tom>
   </div>
 </template>
 
 <script>
-import TodoHeader from "./components/TodoHeader.vue";
-import TodoMain from "./components/TodoMain.vue";
-import TodoFooter from "./components/TodoFooter.vue";
+import BotTom from "./components/BotTom.vue";
+import FormFn from "./components/FormFn.vue";
+import HeadFn from "./components/HeadFn.vue";
 
 export default {
-  components: { TodoHeader, TodoMain, TodoFooter },
+  components: { BotTom, FormFn, HeadFn },
   data() {
-    /*  第一步,铺设数据
-    1.通过父传子的方式 把数据传过去
-    2.子组件props 接受数据
-    2.使用v-for 来渲染数据
-    3.父组件传数据
-   */
     return {
-      list: JSON.parse(localStorage.getItem("list")) || [],
-      getSel: "all",
+      list: [],
     };
   },
-  methods: {
-    //添加
-    addFn(val) {
-      const id = this.list[this.list.length - 1]
-        ? this.list[this.list.length - 1].id + 1
-        : 100;
-      this.list.push({
-        name: val,
-        isDone: false,
-        id,
-      });
-    },
-    //删除
-    delFn(id) {
-      const index = this.list.findIndex((ele) => ele.id == id);
-      this.list.splice(index, 1);
-    },
-    //切换数据
-    fliterdataFn(val) {
-      console.log(val);
-      this.getSel = val;
-    },
-    //清空以=已完成
-    dele() {
-      this.list = this.list.filter((ele) => ele.isDone == false);
-    },
-    setcheckedFn(val) {
-      this.list.forEach((ele) => (ele.isDone = val));
-    },
-  },
-  computed: {
-    //剩余数
-    count() {
-      return this.list.filter((ele) => !ele.isDone).length;
-    },
-    //显 示
-    showlist() {
-      if (this.getSel == "no") {
-        return this.list.filter((ele) => !ele.isDone);
-      } else if (this.getSel == "yes") {
-        return this.list.filter((ele) => ele.isDone);
-      } else {
-        return this.list;
-      }
-    },
-  },
-  watch: {
-    list: {
-      deep: true,
-      handler(val) {
-        localStorage.setItem("list", JSON.stringify(val || []));
-      },
-    },
+  mounted() {
+    this.$axios({
+      url: "/api/getbooks",
+    }).then((res) => (this.list = res.data.data));
   },
 };
 </script>
 
-<style lang="less" scoped>
+<style>
 </style>
---
+++ src/components/HeadFn.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="header" class="in">
+    <input type="text" placeholder="搜索-书本名称" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.in {
+  padding-left: 50px;
+}
+input {
+  margin-bottom: 10px;
+  height: 20px;
+  border: 1px solid #c5ccd3;
+  border-radius: 3px;
+  color: #757575;
+  font-size: 12px;
+}
+input::placeholder {
+  padding-left: 10px;
+}
+</style>
